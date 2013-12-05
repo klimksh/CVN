8f70fb15658c27df4e329ef3c4c85b4f1e76a7c6
Milyash <viola-33@ya.ru>
2013-12-05 13:24:13 +0300

    retrieving info about video from YouTube

---
+++ website/app/controllers/VideoController.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * Created by Milya on 04.12.13.
+ */
+public class VideoController extends Controller {
+
+    public static void getAllVideos() {
+    }
+
+    public static void addVideo() {
+        render();
+    }
+
+    public static void saveVideo() {
+    }
+}
