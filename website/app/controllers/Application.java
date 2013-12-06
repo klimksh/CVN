090d7fdbe4d1998b17a59838fd6d8db8f5cf675e
Milyash <viola-33@ya.ru>
2013-12-07 02:34:57 +0300

    Application.java class reformat

--- website/app/controllers/Application.java
+++ website/app/controllers/Application.java
@@ -1,18 +1,15 @@
 package controllers;
 
-import play.*;
+import Model.User;
+import controllers.securesocial.SecureSocial;
 import play.data.validation.Email;
 import play.data.validation.Equals;
 import play.data.validation.Required;
-import play.mvc.*;
-import controllers.securesocial.SecureSocial;
-
-import java.util.*;
-
-import Model.User;
+import play.mvc.Controller;
+import play.mvc.With;
 
 //import models.*;
-@With( SecureSocial.class )
+@With(SecureSocial.class)
 public class Application extends Controller {
 
     public static void index() {
@@ -22,64 +19,59 @@ public class Application extends Controller {
     // checking if email and password are OK
     public static void authenticate(String email, String password) {
         User user = User.findByEmail(email);
-       if (user == null || !user.checkPassword(password)) {
-        	System.out.println("not successfully");
+        if (user == null || !user.checkPassword(password)) {
+            System.out.println("not successfully");
             flash.error("Bad email or bad password");
             flash.put("email", email);
             // redirect somewhere
-        } 
+        }
         connect(user);
         flash.success("Welcome %s !", user.name);
         //redirect somewhere
     }
 
-      // creating a session
-      static void connect(User user) {
+    // creating a session
+    static void connect(User user) {
         session.put("logged", user.id);
     }
 
-      // get the user which is logged ON
-      static User connectedUser() {
-	        String userId = session.get("logged");
-	        return userId == null ? null : (User) User.findById(Long.parseLong(userId));
-	    }
-      public static void login() {
-	      	  //render();
-	    }
+    // get the user which is logged ON
+    static User connectedUser() {
+        String userId = session.get("logged");
+        return userId == null ? null : (User) User.findById(Long.parseLong(userId));
+    }
 
-      //logout
-      public static void logout() {
+    public static void login() {
+        //render();
+    }
 
-	        flash.success("You've been logged out");
-	        session.clear();
-	       //redirect to main page or to logout page
-      }
+    //logout
+    public static void logout() {
 
-      //register  ( we jhave two password fields  password and verify password
-      public static void register(@Required String name, @Required @Email String email, @Required String password , @Equals("password") String password2) {
-	        if (validation.hasErrors() || User.findByEmail(email)!=null) {
-	            validation.keep();
-	            params.flash();
-	            String error="";
-	            if (User.findByEmail(email)!=null)
-	            {
-	            	// tell him that this ID already exists
-	            }
-	            else
-	            {
-	            	// tell to user that his email or paswword does not match
-	            }                 		            	
-	     
-	           
-	        }
-	        else
-	        {
-             User user= new User(email, password2, name);	         
-	        user.save();
-	        // redirect somewhere
-	        
-	       
-	        }
-	       
-	    }
+        flash.success("You've been logged out");
+        session.clear();
+        //redirect to main page or to logout page
+    }
+
+    //register  ( we jhave two password fields  password and verify password
+    public static void register(@Required String name, @Required @Email String email, @Required String password, @Equals("password") String password2) {
+        if (validation.hasErrors() || User.findByEmail(email) != null) {
+            validation.keep();
+            params.flash();
+            String error = "";
+            if (User.findByEmail(email) != null) {
+                // tell him that this ID already exists
+            } else {
+                // tell to user that his email or paswword does not match
+            }
+
+        } else {
+            User user = new User(email, password2, name);
+            user.save();
+            // redirect somewhere
+
+
+        }
+
+    }
 }
