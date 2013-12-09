7767d2e9f0b59a8d3cf8534143c233b09a4ac2ed
Milyash <viola-33@ya.ru>
2013-12-09 03:57:36 +0300

    main.html format, delete duplicate files

---
+++ website/public/js/google-plus-script-init.js
@@ -0,0 +1,12 @@
+/**
+ * Created by D Mak on 09.12.13.
+ */
+
+(function () {
+    var po = document.createElement('script');
+    po.type = 'text/javascript';
+    po.async = true;
+    po.src = 'https://plus.google.com/js/client:plusone.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(po, s);
+})();
