8469a5a2c9757c4a3fe6f594d6302f01e644c57f
Milyash <viola-33@ya.ru>
2014-01-04 16:00:51 +0300

    Revert "nothiung"
    This reverts commit 565a50d25be1908f9afd7ca16d2f55114e8d3700.

--- website/app/controllers/Application.java
+++ website/app/controllers/Application.java
@@ -5,6 +5,8 @@ import play.data.validation.Email;
 import play.data.validation.Equals;
 import play.data.validation.Required;
 import play.mvc.*;
+import controllers.securesocial.SecureSocial;
+
 import java.util.*;
 
 import Model.User;
