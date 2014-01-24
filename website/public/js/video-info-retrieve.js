66714532b92db893b2e839311354a0914ef82141
Klim Shumaev <klimksh@gmail.com>
2014-01-24 23:08:38 +0100

    video sharing fixed

--- website/public/js/video-info-retrieve.js
+++ website/public/js/video-info-retrieve.js
@@ -2,54 +2,62 @@
  * Created by D Mak on 06.12.13.
  */
 $(document).ready(function () {
-    $("#video-link").change(function () {
+    $("#video-link").keyup(function () {
         var link = $(this).val();
-        link = link.match(/^(?:http:\/\/www.youtube.com\/watch\?v=|http:\/\/youtu.be\/)(\w{11})$/)[1];
-        var request_link = "https://gdata.youtube.com/feeds/api/videos/" + link + "?v=2";
-        $("#video").append('<img src="/public/img/loading.gif"/>');
-        $.ajax({
-            type: "GET",
-            url: request_link,
-            dataType: "xml",
-            success: function (data) {
-                $xml = $(data);
-                var title = $xml.find("title");
-                $("#video-title").val(title.text());
-                $("#video-title").prop('disabled', false);
+        if (link.length>0){
+            link = link.match(/^(?:http:\/\/www.youtube.com\/watch\?v=|http:\/\/youtu.be\/)(\w{11})$/)[1];
+            var request_link = "https://gdata.youtube.com/feeds/api/videos/" + link + "?v=2";
+            $("#video").append('<img src="/public/img/loading.gif"/>');
+            $.ajax({
+                type: "GET",
+                url: request_link,
+                dataType: "xml",
+                success: function (data) {
+                    $xml = $(data);
+                    var title = $xml.find("title");
+                    $("#video-title").val(title.text());
+                    $("#video-title").prop('disabled', false);
 
-                var desc = $xml.find("media\\:description, description");
-                $("#video-description").val(desc.text());
-                $("#video-description").prop('disabled', false);
+                    var desc = $xml.find("media\\:description, description");
+                    $("#video-description").val(desc.text());
+                    $("#video-description").prop('disabled', false);
 
-                var tags = $xml.find("media\\:keywords, keywords");
-                $("#video-tags").val(tags.text());
-                $("#video-tags").prop('disabled', false);
+                    var tags = $xml.find("media\\:keywords, keywords");
+                    $("#video-tags").val(tags.text());
+                    $("#video-tags").prop('disabled', false);
 
-                var thumbnail = $xml.find("media\\:thumbnail[yt\\:name='hqdefault'], thumbnail").attr("url");
-                $("#video-thumbnail").val(thumbnail);
-                //console.log(thumbnail);
-                //$("#video").css("background-image", "url(" + thumbnail+ ")");
-                $("#video-id").val(link);
+                    var thumbnail = $xml.find("media\\:thumbnail[yt\\:name='hqdefault'], thumbnail").attr("url");
+                    $("#video-thumbnail").val(thumbnail);
 
-                $("#loading").remove();
+                    $("#video-id").val(link);
 
-
-                var player;
-                player = new YT.Player('player', {
-                    height: '360',
-                    width: '480',
-                    videoId: '' + link + '',
-                    events: { }
-                });
+                    $("#loading").remove();
 
 
-            },
-            statusCode: {
-                400: function() {
-                    $("#video-link-group").addClass("has-error");
+                    var player;
+                    player = new YT.Player('player', {
+                        height: '360',
+                        width: '480',
+                        videoId: '' + link + '',
+                        events: { }
+                    });
+
+
+                },
+                statusCode: {
+                    400: function() {
+                        $("#video-link-group").addClass("has-error");
+                    }
                 }
-            }
-        })
+            })
+        }
+        else {
+            $(".modal-body form input:text").each(function(){this.value = ""; if (this.id != "video-link"){this.disabled=true;}});
+            $(".modal-body form textarea").each(function(){this.value = "";this.disabled=true;});
+            $("#video-info").empty();
+            $("#video-info").append("<div id='player'></div>");
+            
+        }
     })
 
 
