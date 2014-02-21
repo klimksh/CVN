6ec315e1783f43d6916e4b8c237c755f904f1568
Klim Shumaev <klimksh@gmail.com>
2014-02-21 13:15:29 +0100

    refactoring, changing unittestss

--- website/app/Bootstrap.java
+++ website/app/Bootstrap.java
@@ -10,25 +10,21 @@ import play.test.Fixtures;
 
 @OnApplicationStart
 public class Bootstrap extends Job {
-	public void doJob()
-	{
-			 Fixtures.loadModels("data-users.yml");
-				Logger.info("Users are loaded");
-				Fixtures.loadModels("data-tags.yml");
-				Logger.info("Tags are loaded");
-				Fixtures.loadModels("data-notes.yml");
-				Logger.info("Notes are loaded");
-				Fixtures.loadModels("data-videos.yml");
-				Logger.info("Videos are loaded");
-				List<Note> notes = Note.findAll();
-				Video a = Video.findById(1L);
-				for (Note no : notes) {
-					no.video = a;
-					no.save();
-				}
-				
-
-		
+	public void doJob() {
+		Fixtures.loadModels("data-users.yml");
+		Logger.info("Users are loaded");
+		Fixtures.loadModels("data-tags.yml");
+		Logger.info("Tags are loaded");
+		Fixtures.loadModels("data-notes.yml");
+		Logger.info("Notes are loaded");
+		Fixtures.loadModels("data-videos.yml");
+		Logger.info("Videos are loaded");
+		List<Note> notes = Note.findAll();
+		Video a = Video.findById(1L);
+		for (Note no : notes) {
+			no.video = a;
+			no.save();
+		}
 	}
 
 }
--- website/test/SearchTest.java
+++ website/test/SearchTest.java
@@ -10,53 +10,49 @@ import play.Logger;
 import play.test.Fixtures;
 import play.test.UnitTest;
 
-
 public class SearchTest extends UnitTest {
-	@Before
-   public void dataLoading()
-   {
-	   
-   }
+
+//	@Before
+//	public void dataLoading() {
+//	}
+
 	@Test
-	public void searchVideoQuery()
-	{
-		int a=Video.searchQuery("lorem").size();
-		assertEquals(true, a>0);
+	public void searchVideoQuery() {
+		int a = Video.searchQuery("Test video").size();
+		assertEquals(true, a > 0);
 	}
+
 	@Test
-	public void testSearchOnlyNotes()
-	{
-		int a=Note.searchOnlyNotes("understood").size();
-		assertEquals(1, a);
-		
-	}
-	@Test
-	public void testSearchNotesInAVideo()
-	{
-		int a= Note.searchNotes("understood", 1).size();
+	public void testSearchOnlyNotes() {
+		int a = Note.searchOnlyNotes("test").size();
 		assertEquals(1, a);
 
 	}
-	@Test 
-	public void testWithNoNotes()
-	{
-		int a=Video.searchQuery("webApplicationmistake").size();
+
+	@Test
+	public void testSearchNotesInAVideo() {
+		int a = Note.searchNotes("test", 1).size();
+		assertEquals(1, a);
+
+	}
+
+	@Test
+	public void testWithNoNotes() {
+		int a = Video.searchQuery("test").size();
 		assertEquals(0, a);
-		
+
 	}
+
 	@Test
-	public void testmyVideos()
-	{
-	   int a=Video.myVideos().size();
-	   assertEquals(0, a);
+	public void testmyVideos() {
+		int a = Video.myVideos().size();
+		assertEquals(0, a);
 	}
+
 	@Test
-	public void testmyNotes()
-	{
-	   int a=Note.myNotes().size();
-	   assertEquals(0, a);
+	public void testmyNotes() {
+		int a = Note.myNotes().size();
+		assertEquals(0, a);
 	}
-	
-	
-  
+
 }
